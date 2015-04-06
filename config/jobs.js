20e56d0fffbfd1bcd38dbb1804609a1d14bb9531
Ian Kidd <ian@maxxpotential.com>
2015-04-06 12:16:06 -0400

    adding back job scheduling...

--- config/jobs.js
+++ config/jobs.js
@@ -6,17 +6,17 @@
  * https://github.com/vbuzzano/sails-hook-jobs
  */
 
-//module.exports.jobs = {
+module.exports.jobs = {
 
-  // Where are jobs files
-  //"jobsDirectory": "api/jobs",
-  //"db": { 
-  //  "address"    : process.env.DB_URL, //localhost:27017/jobs process.env.DB_URL
-  //  "collection" : "agendaJobs" 
-  //},
-  //"name": "process name",
-  //"processEvery": "10 seconds",
-  //"maxConcurrency": 20,
-  //"defaultConcurrency": 5,
-  //"defaultLockLifetime": 10000
-//};
+  //Where are jobs files
+  "jobsDirectory": "api/jobs",
+  "db": { 
+   "address"    : process.env.DB_URL, //localhost:27017/jobs process.env.DB_URL
+   "collection" : "agendaJobs" 
+  },
+  "name": "process name",
+  "processEvery": "10 seconds",
+  "maxConcurrency": 20,
+  "defaultConcurrency": 5,
+  "defaultLockLifetime": 10000
+};
