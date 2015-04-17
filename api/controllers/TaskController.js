f6555110d46b5244387844429527503c5aae90c3
Ian Kidd <ian@maxxpotential.com>
2015-04-17 13:09:19 -0400

    commit

--- api/controllers/TaskController.js
+++ api/controllers/TaskController.js
@@ -66,9 +66,11 @@ module.exports = {
 	subscribe: function(req, res) {
 		var socket = req.socket;
 		console.log(socket);
-    	//var io = sails.io;
-		var io = socket.io;
-		io.socket.get('/task/subscribeToTasks'); 
+    	var io = sails.io;
+		//var io = socket.io;
+		// io.socket.get('/task/subscribeToTasks');
+		socket.join('task');
+		socket.broadcast.to('task').emit('messageName', {thisIs: 'theMessage'}); 
 	},
 
 	subscribeToTasks: function(req, res) {
