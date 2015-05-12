316c04e0822d68ded59d35f737b0c2b1a342dd27
ian <ian@hourwise.com>
2015-05-12 16:51:02 -0400

    removing attempt at css minification

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -15,11 +15,7 @@
 // (if you're using LESS with the built-in default config, you'll want
 //  to change `assets/styles/importer.less` instead.)
 var cssFilesToInject = [
-  'styles/**/*.css',
-  'css/animate.css',   
-  'css/style.css'
-  
-
+  'styles/**/*.css'
 ];
 
 
