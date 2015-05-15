ee8f390331867b7b8e6910a2d44a326defabd13d
ian <ian@hourwise.com>
2015-05-15 10:49:14 -0400

    fixing error

--- api/controllers/StaticController.js
+++ api/controllers/StaticController.js
@@ -48,10 +48,10 @@ module.exports = {
 
     var taskOwner = null;
 
-    if (req.param('companyRep') != null)
+    if (req.param('companyRep') != 'null')
       taskOwner = req.param('companyRep');
 
-    else if (req.param('hourwiseRep') != null)
+    else if (req.param('hourwiseRep') != 'null')
       taskOwner = req.param('hourwiseRep');
 
     Task.create({'name': taskName, 'description': taskDescription, 'status': 'open', 'category': taskCategory, 'owner': taskOwner}, function (err, task) {
@@ -63,9 +63,11 @@ module.exports = {
               var uuid = require('node-uuid');
 
               var alertId = uuid.v4();
-              user.addAlert(taskName, alertId, task.id, true);
-              User.publishUpdate(taskOwner, { message: taskName, id: alertId, communicationId: task.id, fromTask: true  });
-
+              if (user != null) {
+                user.addAlert(taskName, alertId, task.id, true);
+                User.publishUpdate(taskOwner, { message: taskName, id: alertId, communicationId: task.id, fromTask: true  });
+              }
+              
               Mandrill.sendEmail({'toEmail': 'support@hourwise.com', 'toName': 'Hourwise Support', 'fromEmail': req.param('email'), 'subject': taskName, 'body': taskDescription}, function (err) {
                 res.send(200);
               });
