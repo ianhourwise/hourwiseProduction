9ecceeda9487a1e175b18b5610a41b6f6c7ad90e
ian <ian@hourwise.com>
2015-05-28 11:42:49 -0400

    still trying to get newrelic to work again...

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -13,8 +13,9 @@ module.exports.bootstrap = function(cb) {
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  sails.services.passport.loadStrategies();
   var newrelic = require('newrelic');
+  sails.services.passport.loadStrategies();
+
   //Jobs.schedule('everyday at 6:30am', 'Nutshell', {});
 
  //  	var drone = require('schedule-drone');
