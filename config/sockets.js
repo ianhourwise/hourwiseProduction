421bffaad0ca6e625e9944a4527f6f1abc9afaf9
ian <ian@hourwise.com>
2015-05-28 10:16:31 -0400

    forgot to save sockets.js file

--- config/sockets.js
+++ config/sockets.js
@@ -12,10 +12,9 @@
 
 module.exports.sockets = {
 
-  // transports: [
-  //   'websocket',
-  //   'xhr-polling'
-  // ],
+  transports: [
+    'websocket'
+  ],
    
    // transports: [
    //  'websocket',
