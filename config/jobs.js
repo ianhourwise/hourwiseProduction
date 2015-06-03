5d1e800867dab39cef6fbaaf5f6d67e310999de9
ian <ian@hourwise.com>
2015-06-03 16:17:50 -0400

    hopefully getting rid of the jobs hook...

--- config/jobs.js
+++ config/jobs.js
@@ -9,16 +9,16 @@
 module.exports.jobs = {
 
   //Where are jobs files
-  "jobsDirectory": "api/jobs",
-  "db": { 
+  // "jobsDirectory": "api/jobs",
+  // "db": { 
     //alternate address for dev and production!
-   "address" : process.env.DB_URL,
+   //"address" : process.env.DB_URL,
    //"address" :  "localhost:27017/jobs",
-   "collection" : "agendaJobs" 
-  },
-  "name": "process name",
-  "processEvery": "3 seconds",
-  "maxConcurrency": 15,
-  "defaultConcurrency": 10,
-  "defaultLockLifetime": 10000
+  //  "collection" : "agendaJobs" 
+  // },
+  // "name": "process name",
+  // "processEvery": "3 seconds",
+  // "maxConcurrency": 15,
+  // "defaultConcurrency": 10,
+  // "defaultLockLifetime": 10000
 };
