6c4928a7efb3ed78a3650507dc5c612854d81601
Jon Hill <jonhill04@gmail.com>
2015-06-03 10:59:55 -0400

    attempt to fix broken

---
+++ api/models/CompanyRelationship.js
@@ -0,0 +1,26 @@
+/**
+* Company_relationships.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  connection: ['someMongodbServer'], 
+
+  attributes: {
+  	company:{model: 'company'},
+
+    employee:{model: 'user'},
+
+    companyRole: {
+    	type:'string',
+    	enum: ['admin', 'manager', 'technician'],
+    	defaultsTo: 'technician'
+	}
+
+
+
+  }
+};
